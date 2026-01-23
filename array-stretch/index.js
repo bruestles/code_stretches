199b9ee8546d88873062c0be07c3a90c7afab839
Stacey Bruestle <bruestle@umich.edu>
2026-01-23 10:09:11 -0500

    adding array-stretch to repo

---
+++ array-stretch/index.js
@@ -0,0 +1,13 @@
+const bunnyNames = [
+  "Fluffy",
+  "Benny",
+  "Cafe Au-lait",
+  "Cuddles",
+  "Herby",
+  "Fred",
+];
+
+function addIsTheBest(bunnyArray) {
+  for (let bunny of bunnyArray) console.log(`${bunny} is the best!`);
+}
+addIsTheBest(bunnyNames);
